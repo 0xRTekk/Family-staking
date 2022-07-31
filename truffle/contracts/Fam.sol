6933941af6ad277979dc48e1ff35a143989c7ec9
maudenahutchinson <maudenahutchinson@gmail.com>
2022-07-31 22:04:30 +0200

    completion du contract ERC20 FAM

--- truffle/contracts/Fam.sol
+++ truffle/contracts/Fam.sol
@@ -9,6 +9,12 @@ contract Fam is ERC20, Ownable {
 
 	
 
+	// Events
+
+	event FamTransfered(address recipient, uint amount);
+    
+
+
 	constructor() ERC20('Family Token', 'FAM') {}
 	
 	/* 
@@ -16,6 +22,7 @@ contract Fam is ERC20, Ownable {
 	**/
 	function faucet(address recipient, uint amount) external onlyOwner {
 		_mint(recipient, amount);
+		
 	}
     /* 
 	@dev replace default 18 decimals to 8 decimals
@@ -23,7 +30,21 @@ contract Fam is ERC20, Ownable {
 	function decimals() public view virtual override returns (uint8){
 		return 8;
 	}
+    /* 
+	* @dev See {IERC20-transfer} Openzeppelin librairy
+     *
+     * Requirements:
+     *
+     * - `to` cannot be the zero address.
+     * - the caller must have a balance of at least `amount`.
+	**/
+   function transferFAM(address recipient, uint amount) public{
+       transfer(recipient , amount);
+	   emit FamTransfered(recipient, amount);
+   }
 
-
-    
+   function balanceOfFAM(address account)public view returns(uint){
+      balanceOf(account);
+   }
+   
 }
