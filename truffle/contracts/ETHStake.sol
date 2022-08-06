9dbe9d0db36748a7e8aa0a1aec6f9765a7c11909
Yohann Youssouf <y.youssouf@protonmail.com>
2022-08-06 17:00:44 +0200

    WIP rewards ETH

--- truffle/contracts/ETHStake.sol
+++ truffle/contracts/ETHStake.sol
@@ -14,28 +14,44 @@ contract ETHStake {
         uint depositDate;
     }   
 
+    uint minDeposit = 10000000000000000;
+
     // Events
-    event DepositRegistered(address userAddress, uint amount, uint pendingReward);
+    event DepositRegistered(address userAddress, uint amount);
     event WithdrawRegistered(address userAddress, uint amount);
+    event UpdatedRewards(address userAddress, uint amount);
 
     /**
-     * @dev Internal function that register the stake. Used by the stake function and the default receive function. Emits a StakeRegistered event.
+     * @dev Internal function that register the stake and calculate pending rewards. Used by the stake function and the default receive function. Emits a StakeRegistered event.
      */ 
     function registerDeposit() internal {
+        require(msg.value >= minDeposit, "Minimum deposit is 0.01 ETH");
+
         uint currentStake = stakes[msg.sender].amount;
         uint currentDepositDate = stakes[msg.sender].depositDate;
         // Updating the user stake first
         stakes[msg.sender].amount = currentStake + msg.value;
         stakes[msg.sender].depositDate = block.timestamp;
         // We need to calculate the pending reward amount and store it
-        // calculating nbDay since deposit
-        uint nbDay = (block.timestamp - currentDepositDate) / 60 / 60 / 24;
-        // APR = 3.9% ETH / AN soit 0.01% ETH/jour
-        uint dummyETHValue = 1980;
-        uint reward = (nbDay * (dummyETHValue*100)) / 100;
-        pendingRewards[msg.sender] += reward;                
+        if(currentStake > 0) {
+            updatePendingRewards(currentStake, currentDepositDate, msg.sender);
+        }        
         
-        emit DepositRegistered(msg.sender, msg.value, reward);
+        emit DepositRegistered(msg.sender, msg.value);
+    }
+
+    function updatePendingRewards(uint _stakes, uint _from, address _userAddress) internal{
+        // calculating nbDay since deposit
+        uint nbDay = (block.timestamp - _from) / 60 / 60 / 24;
+        // APR = 3.9%/AN soit 0.01% jour
+        uint dummyETHValue = 1980;
+        uint userStakeValue = dummyETHValue/_stakes;
+
+        // Calculating reward and updating the user account
+        uint reward = (nbDay * (userStakeValue*100)) / 100;
+        pendingRewards[_userAddress] += reward;
+
+        emit UpdatedRewards(_userAddress, reward);
     }
 
     /**
@@ -44,6 +60,14 @@ contract ETHStake {
      */ 
     function getBalance(address _userAddress) view external returns(uint){
         return stakes[_userAddress].amount;
+    }
+
+    /**
+     * @notice Return a user pending reward balance
+     * @param _userAddress address of the user
+     */
+    function getPendingRewards(address _userAddress) view external returns(uint){
+        return pendingRewards[_userAddress];
     }
 
     /**
@@ -60,18 +84,29 @@ contract ETHStake {
      * @param _amount uint - Number of wei to withdraw
      */
     function withdraw(uint _amount) external {
+        // verifications on the user balance
         uint userStake = stakes[msg.sender].amount;
-        uint timeLock = stakes[msg.sender].depositDate + 2 days;
-
         require(userStake != 0, "Account is empty");
-        require(block.timestamp >= timeLock, "Deposit is time locked");
         require(userStake >= _amount, "Cannot withdraw more than your current balance");
         require(address(this).balance >= _amount);
+        // Storing the current date of reference and calculating the lock release
+        uint referenceDate = stakes[msg.sender].depositDate;
+        uint timeLock = referenceDate + 2 days;
+        require(block.timestamp >= timeLock, "Deposit is time locked");
 
+        // Updating the user's stake
         stakes[msg.sender].amount = userStake - _amount;
+        // Calculating and updating the pending rewards amount
+        updatePendingRewards(userStake, referenceDate, msg.sender);
+        uint rewardsToSend = pendingRewards[msg.sender];
+        pendingRewards[msg.sender] = 0;
+        // TODO Minting the rewardsToSend to the user
+
+        // Transfering back the amount to the user
+        payable(msg.sender).transfer(_amount);
+        // TODO Error management
 
         emit WithdrawRegistered(msg.sender, _amount);
-        payable(msg.sender).transfer(_amount);
     }
 
     /**
