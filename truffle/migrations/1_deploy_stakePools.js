7d4617674da95d18c99cfb2fd7bedd9b86906e61
Remitekky <remitekky@hotmail.fr>
2022-08-07 17:17:47 +0200

    Adding DAI token and DAI stake contracts to the deply script

--- truffle/migrations/1_deploy_stakePools.js
+++ truffle/migrations/1_deploy_stakePools.js
@@ -1,9 +1,12 @@
 const FAM = artifacts.require("FAM");
+const DAI = artifacts.require("DAI");
 const FAMStake = artifacts.require("FAMStake");
 const ETHStake = artifacts.require("ETHStake");
+const DAIStake = artifacts.require("DAIStake");
 
 module.exports =  async function (deployer, _network) {
-    await deployer.deploy(FAM);
+	// Deploying the FAM token contract
+  await deployer.deploy(FAM);
 	const FAMToken = await FAM.deployed();
 	// Deploying the FAM Staking contract
 	await deployer.deploy(FAMStake, FAMToken.address);
@@ -11,18 +14,15 @@ module.exports =  async function (deployer, _network) {
 	// Deploying the ETH Staking contract
 	await deployer.deploy(ETHStake, FAMToken.address);
 	const ETHStakingContract = await ETHStake.deployed();
-	// Deploying the DAI and DAI Staking Contract
+	// Deploying the DAI token contract
+	await deployer.deploy(DAI);
+	const DAIToken = await DAI.deployed();
+	// Deploying the DAI Staking contract
+	await deployer.deploy(DAIStake, DAIToken.address);
+	const DAIStakingContract = await DAIStake.deployed();
 
 	// Authorizing the staking contract to mint
 	await FAMToken.authorize(FAMStakingContract.address);
 	await FAMToken.authorize(ETHStakingContract.address);
-
-
-	// Misc
-	// await FAMToken.faucet(FAMStakingContract.address, 100);
-
-	// const balance0 = await FAMToken.balanceOf(FAMStakingContract.address);
-	// console.log(balance0.toString());
-    // console.log(FAMToken.address);
-
+	await FAMToken.authorize(DAIStakingContract.address);
 } 
